c923e4bbf426b76f79cdb08b4f4f64e34288eb6a
Ratha <rathavun449@gmail.com>
2026-02-06 13:14:14 +0700

    ChalSophea

---
+++ ChallengeSophea.py
@@ -0,0 +1,20 @@
+import random
+
+# G_num = int(input("Guess the number between 1 - 100: "))
+
+# listMe = list(range(1, 51))
+# random.shuffle(listMe)
+# print (listMe)
+# print (listMe[49])
+
+
+def scannerMe(a:int, b:str):
+    print (a,b)
+
+scannerMe(int(input("input A:")), input("input B:"))
+# random.shuffle(list)
+# print(list)
+
+# mylist = random.shuffle(list)
+# print (mylist)
+
